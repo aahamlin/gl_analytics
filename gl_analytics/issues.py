ddd17eb3f30941b6df7156d15328e894f76195fd
Andrew Hamlin <aahamlin@gmail.com>
2021-03-19 22:14:10 -0400

    retrieve issue label events

--- gl_analytics/issues.py
+++ gl_analytics/issues.py
@@ -14,26 +14,33 @@ class Session(object):
     def get(self):
         raise IssuesError('Not Implemented')
 
+class AbstractRequestFactory(object):
+
+    def get(self, url, **kwargs):
+        raise IssuesError()
+
+class RequestFactory(AbstractRequestFactory):
+
+    def get(self, url, **kwargs):
+        return requests.get(url, **kwargs)
+
 class GitlabSession(Session):
 
-    def __init__(self, access_token=None):
+    def __init__(self, access_token=None, request_factory=RequestFactory()):
         self._access_token = access_token
-
-    def _get(self, url, **kwargs):
-        return requests.get(url, **kwargs)
+        self._request_factory = request_factory
 
     def get(self, url):
         headers = { 'PRIVATE-TOKEN': self._access_token }
-        return self._get(url, headers=headers)
+        return self._request_factory.get(url, headers=headers)
 
-    def build_request_url(self, group, milestone):
-        url = "{0}/groups/{1}/issues".format(GITLAB_URL_BASE,group)
-        params = { 'pagination': 'keyset', 'milestone': milestone }
-        print('built url:', url)
-        print('built params:', params)
-        return "{0}?{1}".format(url, urlencode(params))
 
-class Repository(object):
+class AbstractRepository(object):
+
+    def list(self):
+        raise IssuesError()
+
+class GroupIssuesRepository(AbstractRepository):
     """This is specifically a GitLab Issue repository. Current GitLab version is 13.11.0-pre.
 
     If ever we needed it, an AbstractRepository could be extracted.
@@ -45,19 +52,31 @@ class Repository(object):
         self._session = session
         self._group = group
         self._milestone = milestone
+        self._url = self._build_request_url()
+
+    @property
+    def url(self):
+        return self._url
 
     def list(self):
         """Return issues from the repository.
-
-       """
+        """
         return [x for x in self._page_results()]
+
+    def _build_request_url(self):
+        url = "{0}/groups/{1}/issues".format(GITLAB_URL_BASE, self._group)
+        params = { 'pagination': 'keyset', 'milestone': self._milestone }
+        print('built url:', url)
+        print('built params:', params)
+        return "{0}?{1}".format(url, urlencode(params))
+
 
     def _page_results(self):
         """Generator of issues from pages of results.
        """
 
         # starting request
-        url = self._session.build_request_url(self._group, self._milestone)
+        url = self.url
 
         hasMore = True
         while hasMore:
@@ -70,10 +89,7 @@ class Repository(object):
             count = len(payload)
             print(f'processing {count} items');
             for item in payload:
-                yield {
-                    'iid': item['iid'],
-                    'project_id': item['project_id']
-                }
+                yield Issue(item)
 
             if r1.links and 'next' in r1.links:
                 # setup next page request
@@ -82,3 +98,63 @@ class Repository(object):
                 print('next page:', url)
             else:
                 hasMore = False
+
+class Issue(object):
+
+    def __init__(self, item):
+
+        self._issue_id = item['iid']
+        self._project_id = item['project_id']
+        self._opened_at = item['opened_at']
+        self._closed_at = item.get('closed_at')
+
+    @property
+    def issue_id(self):
+        return self._issue_id
+
+    @property
+    def project_id(self):
+        return self._project_id
+
+    @property
+    def opened_at(self):
+        self._opened_at
+
+    @property
+    def closed_at(self):
+        self._closed_at
+
+class IssueLabelRepository(AbstractRepository):
+
+    def __init__(self, session=None, issue=None):
+        self._session = session
+        self._issue = issue
+        self._url = self._build_request_url()
+
+    @property
+    def url(self):
+        return self._url
+
+    def list(self):
+        return [x for x in self._label_events()]
+
+    def _build_request_url(self):
+        # /api/v4/projects/8279995/issues/191/resource_label_events
+        url = "{0}/projects/{1}/issues/{2}/resource_label_events".format(
+            GITLAB_URL_BASE, self._issue.project_id, self._issue.issue_id)
+        return url
+
+    def _label_events(self):
+
+        url = self.url
+        r = self._session.get(url)
+        r.raise_for_status()
+
+        payload = r.json()
+        print(f'processing label events: {payload}')
+        # action: add, remove
+        # created_at: datetime
+        # label.name
+        # label.id
+        for event in payload:
+            yield {'action': event['action'], 'datetime': event['created_at'], 'step': event['label']['name']}
--- tests/test_issues.py
+++ tests/test_issues.py
@@ -2,54 +2,47 @@ import pytest
 
 import io
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qs
 from requests import Response
 
 import gl_analytics.issues as issues
 
-class FakeSession():
+class FakeRequestFactory(issues.AbstractRequestFactory):
 
-    def __init__(self, responses=None):
+    def __init__(self):
         """Initialize a fake to test requests.
-
-       response: provide a complete HTTP response object
-       cb: callback will be called on every get()
-       """
-        self.path = None
-        self.params = None
-        if isinstance(responses, list):
-            self.responses = responses
-        elif responses:
-            self.responses = [responses]
-        else:
-            resp = Response()
-            resp.status_code = 500
-            self.responses = [resp]
-
-        self.headers = {}
-        self.params = {}
-
+        """
+        self.responses = []
         self._call_index = -1
         self._call_instances = []
 
     def get(self, url, **kwargs):
         self._call_index += 1
-
         o = urlparse(url)
-        self.path = o.path
-        self.params = o.query
-        self.headers = kwargs.get('headers')
-        self._call_instances.append({'url': url, 'headers': self.headers})
+        info = FakeRequestInfo(path=o.path, params=parse_qs(o.query), headers=kwargs.get('headers'))
+        print(info)
+        self._call_instances.append(info)
 
-        return self.responses[self._call_index]
+        try:
+            return self.responses[self._call_index]
+        except IndexError:
+            http500 = Response()
+            http500.status_code = 500
+            return http500
 
     @property
     def call_instances(self):
         return self._call_instances
 
-def build_fake_request(responses=None):
-    fake = FakeSession(responses=responses)
-    return fake
+class FakeRequestInfo(object):
+
+    def __init__(self, path="", params={}, headers={}):
+        self.path = path
+        self.params = params
+        self.headers = headers
+
+    def __str__(self):
+        return f"path:{self.path}, params:{self.params}, headers:{self.headers}"
 
 def build_http_response(status_code, bytes=None, headers=None):
     # simple setup of fake response data
@@ -62,11 +55,6 @@ def build_http_response(status_code, bytes=None, headers=None):
         response.headers.update(headers)
     return response
 
-# XXX apply the fake session to the session object via Mixin?
-def setup_fake_request(session, fake):
-    # install our _get handler for testing
-    session._get = fake.get
-
 def test_abstract_session():
     with pytest.raises(issues.IssuesError):
         issues.Session().get()
@@ -75,20 +63,15 @@ def test_gitlab_session():
     s = issues.GitlabSession(access_token="x")
     assert s != None
 
-def test_gitlab_session_get():
-    s = issues.GitlabSession(access_token="x")
-    resp = build_http_response(
+def test_gitlab_session_adds_access_token():
+    fake = FakeRequestFactory()
+    fake.responses.append(build_http_response(
         200,
-        bytes=io.BytesIO(b'[{"id":8000234,"iid":2,"project_id":"8273019","title":"test title"}]'))
-    fake = build_fake_request(responses=resp)
-    setup_fake_request(s, fake)
-    url = s.build_request_url("gozynta", "mb_v1.3")
-    s.get(url)
+        bytes=io.BytesIO(b'[{"id":8000234,"iid":2,"project_id":"8273019","title":"test title","opened_at":"2021-03-09T17:59:43.041Z"}]')))
 
-    assert fake.path != None
-    assert fake.path.endswith("/groups/gozynta/issues")
-    assert fake.params.endswith("pagination=keyset&milestone=mb_v1.3")
-    assert 'PRIVATE-TOKEN' in fake.headers
+    s = issues.GitlabSession(access_token="x", request_factory=fake)
+    s.get("https://gitlab.com/api/v4/groups/gozynta/issues")
+    assert 'PRIVATE-TOKEN' in fake.call_instances[-1].headers
 
 def test_repo_list_pagination():
     """Make sure we page correctly.
@@ -96,23 +79,47 @@ def test_repo_list_pagination():
     The GitLab API, when using the pagination=keyset parameter, returns the pages referenced
     as first, next, last via the link response header.
     """
-    s = issues.GitlabSession(access_token="x")
+
     resp = []
     resp.append(build_http_response(
         200,
         headers={'link': '<https://gitlab.com/api/v4/groups/gozynta/issues?id=gozynta&milestone=mb_v1.3&non_archived=true&order_by=created_at&page=2&pagination=keyset&per_page=5&sort=desc&state=all&with_labels_details=false>; rel="next", <https://gitlab.com/api/v4/groups/gozynta/issues?id=gozynta&milestone=mb_v1.3&non_archived=true&order_by=created_at&page=1&pagination=keyset&per_page=5&sort=desc&state=all&with_labels_details=false>; rel="first", <https://gitlab.com/api/v4/groups/gozynta/issues?id=gozynta&milestone=mb_v1.3&non_archived=true&order_by=created_at&page=9&pagination=keyset&per_page=5&sort=desc&state=all&with_labels_details=false>; rel="last"'},
-        bytes=io.BytesIO(b'[{"id":8000234,"iid":2,"project_id":"8273019","title":"test title"}]')))
+        bytes=io.BytesIO(b'[{"id":8000234,"iid":2,"project_id":"8273019","title":"test title","opened_at":"2021-03-09T17:59:43.041Z"}]')))
 
     resp.append(build_http_response(
         200,
         headers={'link': '<https://gitlab.com/api/v4/groups/gozynta/issues?id=gozynta&milestone=mb_v1.3&non_archived=true&order_by=created_at&page=1&pagination=keyset&per_page=5&sort=desc&state=all&with_labels_details=false>; rel="first", <https://gitlab.com/api/v4/groups/gozynta/issues?id=gozynta&milestone=mb_v1.3&non_archived=true&order_by=created_at&page=9&pagination=keyset&per_page=5&sort=desc&state=all&with_labels_details=false>; rel="last"'},
-        bytes=io.BytesIO(b'[{"id":8000235, "iid":3,"project_id":"8273019","title":"test title 2"}]')))
+        bytes=io.BytesIO(b'[{"id":8000235, "iid":3,"project_id":"8273019","title":"test title 2","opened_at":"2021-03-09T17:59:43.041Z"}]')))
 
-    fake = build_fake_request(responses=resp)
-    setup_fake_request(s, fake)
+    fake = FakeRequestFactory()
+    fake.responses = resp
+    s = issues.GitlabSession(access_token="x", request_factory=fake)
 
-    repo = issues.Repository(session=s, group="gozynta", milestone="mb_v1.3")
+    repo = issues.GroupIssuesRepository(session=s, group="gozynta", milestone="mb_v1.3")
     issue_list = repo.list()
+
     assert len(fake.call_instances) == 2
-    print('Issue listing', issue_list)
+    assert fake.call_instances[-1].path == "/api/v4/groups/gozynta/issues"
+    assert 'pagination' in fake.call_instances[-1].params
+    assert 'milestone' in fake.call_instances[-1].params
+
     assert len(issue_list) == 2
+    assert issue_list[0] and issue_list[0].issue_id == 2
+    assert issue_list[1] and issue_list[1].issue_id == 3
+
+
+def test_issue_label_repo_list():
+
+    resp = build_http_response(
+        status_code=200,
+        # XXX load payload from string to bytes more easily...
+        bytes=io.BytesIO(b'[{"created_at": "2021-02-09T16:59:37.783Z","resource_type": "Issue","label":{"id": 18205357,"name": "workflow::Designing"},"action": "add"},{"created_at": "2021-02-09T17:00:49.416Z","resource_type": "Issue","label": {"id": 18205410,"name": "workflow::In Progress"},"action": "add"},{"created_at": "2021-02-09T17:00:49.416Z","resource_type": "Issue","label": {"id": 18205357,"name": "workflow::Designing"},"action": "remove"}]'))
+    fake = FakeRequestFactory()
+    fake.responses.append(resp)
+    s = issues.GitlabSession(access_token="x", request_factory=fake)
+
+    repo = issues.IssueLabelRepository(
+        session=s,
+        issue=issues.Issue({'iid': 12, 'project_id': '45232', 'opened_at': '2021-03-09T17:59:43.041Z'}))
+    labels_list = repo.list()
+    assert len(labels_list) == 3
