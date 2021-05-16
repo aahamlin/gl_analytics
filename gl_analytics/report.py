2d54641dd4fc4fb2ecc3c6102b12a6a39b79be74
Andrew Hamlin <aahamlin@gmail.com>
2021-05-15 21:21:30 -0400

    add ylimit by default to plots

--- gl_analytics/report.py
+++ gl_analytics/report.py
@@ -34,7 +34,7 @@ class PlotReport:
     """Configure the output of a DataFrame to plot image (.png).
     """
 
-    def __init__(self, df, file=None, title="CFD", **kwargs):
+    def __init__(self, df, file=None, title="CFD", ylimit=True, **kwargs):
         """Prepare a plot file.
 
         This is no more than a convenience wrapper to Pandas DataFrame API.
@@ -43,6 +43,7 @@ class PlotReport:
         df - DataFrame (required)
         file - Write to given file path or open file buffer. Returns a string when None.
         title - Extra title, typically represents the search criteria, e.g. milestone name
+        ylimit - If True, limit the y-axis to total excluding the last category
         """
         self._df = df
         self._file = file
@@ -52,13 +53,22 @@ class PlotReport:
             str(df.index.date[-1])
         ])
 
+        last_col = df.columns.values[-1]
+        self.ylimit = df.drop(last_col, axis=1).sum(axis=1).max() if ylimit else False
+
     def export(self):
         plt.close("all")
-        ax = self._df.plot.area(
-            title=self.title,
-            legend="reverse",
-            ylabel="Count of Issues",
-            xlabel="Days"
-        )
+
+        args = {
+            "title": self.title,
+            "legend": "reverse",
+            "ylabel": "Count of Issues",
+            "xlabel": "Days"
+        }
+
+        if self.ylimit:
+            args["ylim"] = (0, self.ylimit)
+
+        ax = self._df.plot.area(**args)
         fig = ax.get_figure()
         fig.savefig(self._file)
